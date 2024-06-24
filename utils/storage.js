decf6b5b599298a9832b326d4c3f4d12353e6d87
SarikViper <dzamolovsardor5@gmail.com>
2024-06-24 16:03:31 +0500

    first commit

---
+++ utils/storage.js
@@ -0,0 +1,26 @@
+class Storage {
+
+	getItem(name) {
+		const json = localStorage.getItem(name);
+
+		return JSON.parse(json) || [];
+	}
+
+	setItem(name, data) {		
+		const arr = this.getItem(name);
+
+		arr.push(data);
+
+		localStorage.setItem(name, JSON.stringify(arr));
+
+		return arr;
+	}
+
+	auth (user) {
+		const users = this.getItem('users');
+
+		console.log(user);
+		console.log(users.find((elem) => elem.email === user.email && elem.password === user.password));
+		return Boolean(users.find((elem) => elem.email === user.email && elem.password === user.password));
+	}
+}
